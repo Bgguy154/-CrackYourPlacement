2f239ce94f5f8cd4992287d497cfb97eee0a714f
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2024-08-12 20:43:54 +0530

    Time: 21 ms (29.54%), Space: 28.1 MB (12.69%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int candidate = 0, count = 0;
-
-        for (int num : nums) {
-            if (count == 0) {
-                candidate = num;
-            }
-            count += (num == candidate) ? 1 : -1;
-        }
-
-        return candidate;
+       std::sort(nums.begin(), nums.end());
+        
+        // Return the middle element
+        return nums[nums.size() / 2];
     }
 };
