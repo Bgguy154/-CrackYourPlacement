5051eb569e34ab291492a077cdaee72e873aef6a
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2024-09-08 12:35:56 +0530

    Time: 6 ms (92.18%), Space: 16.4 MB (57.25%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -11,55 +11,67 @@
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        // ListNode* first=new ListNode(0);
-        // while(first->next){
-        //     int x=k;
-        //     ListNode* second=first->next;
-        //     ListNode* prev=first;
-        //     ListNode*curr=first->next;
-        // while(curr&&x){
-        //     first=curr->next;
-        //     curr->next=prev;
-        //     prev=curr;
-        //     curr=first;
-        //     x--;
-        // }
-        // first->next=prev;
-        // second->next=curr;
-        // first=second;
-        // }
-        // first=head;
-        // head=head->next;
-        // delete first;
-        // return head;
-          if (!head || k == 1)
-      return head;
+//   next stores the node after curr.
+// Update curr->next to skip next and point to next->next (effectively removing next from its original position).
+// Insert next at the beginning of the group by updating next->next to point to prev->next.
+// Finally, update prev->next to next, effectively placing next as the new head of the reversed group
 
-    const int length = getLength(head);
-    ListNode dummy(0, head);
-    ListNode* prev = &dummy;
-    ListNode* curr = head;
+        
+        //           if (!head || k == 1)
+//       return head;
 
-    for (int i = 0; i < length / k; ++i) {
-      for (int j = 0; j < k - 1; ++j) {
-        ListNode* next = curr->next;
-        curr->next = next->next;
-        next->next = prev->next;
-        prev->next = next;
-      }
-      prev = curr;
-      curr = curr->next;
-    }
+//     const int length = getLength(head);
+//     ListNode dummy(0, head);
+//     ListNode* prev = &dummy;
+//     ListNode* curr = head;
 
-    return dummy.next;
-  }
+//     for (int i = 0; i < length / k; ++i) {
+//       for (int j = 0; j < k - 1; ++j) {
+//         ListNode* next = curr->next;
+//         curr->next = next->next;
+//         next->next = prev->next;
+//         prev->next = next;
+//       }
+//       prev = curr;
+//       curr = curr->next;
+//     }
 
- private:
-  int getLength(ListNode* head) {
-    int length = 0;
-    for (ListNode* curr = head; curr; curr = curr->next)
-      ++length;
-    return length;
+//     return dummy.next;
+//   }
+
+//  private:
+//   int getLength(ListNode* head) {
+//     int length = 0;
+//     for (ListNode* curr = head; curr; curr = curr->next)
+//       ++length;
+//     return length;
        
-    }
+//     }
+        
+        if (!head || k==1)return head;
+        const int l=getLength(head);
+        ListNode dummy(0,head);
+        ListNode* curr=head;
+        ListNode*prev=&dummy;
+        
+        for(int i=0;i<l/k;i++){
+            for(int j=0;j<k-1;++j){
+                ListNode* next=curr->next;
+                curr->next=next->next;
+                next->next=prev->next;
+                prev->next=next;
+            }
+            prev=curr;
+            curr=curr->next;
+        }
+            return dummy.next;
+        }
+        
+        private:
+        int getLength(ListNode* head){
+            int le=0;
+            for(ListNode*curr=head;curr;curr=curr->next)
+                ++le;
+                return le;
+        }
 };
