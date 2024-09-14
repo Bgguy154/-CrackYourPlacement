7ed19ec493ea56987f7392201cde3d469c7c5fc5
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2024-09-14 11:41:37 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (40.71%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<vector<int>> subsets(vector<int>& nums) {
+          ans.clear();
+        queue<pair<vector<int>, int>> q;
+        q.push({{}, 0});
+        while(!q.empty()){
+            auto k = q.front();
+            q.pop();
+            auto vec = k.first;
+            auto idx = k.second;
+            if(idx >= nums.size()){
+                ans.push_back(vec);
+            }else{
+                q.push({vec, idx + 1});
+                vec.push_back(nums[idx]);
+                q.push({vec, idx + 1});
+            }
+        }
+
+        return ans;
+    }
+};
