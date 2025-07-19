c9414f98d95dc87e4c4b361315b44499046256ec
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2025-07-19 19:48:21 +0530

    Time: 0 ms, Memory: 58 MB - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.js
+++ merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -13,15 +13,15 @@
  */
 var mergeTwoLists = function(list1, list2) {
     // Base cases
-    if (!list1) return list2;
-    if (!list2) return list1;
-
-    // Recursive case
-    if (list1.val <= list2.val) {
-        list1.next = mergeTwoLists(list1.next, list2);
+   if(!list1) return list2;
+    if(!list2) return list1;
+    
+    if(list1.val<=list2.val){
+        list1.next= mergeTwoLists(list1.next,list2);
         return list1;
-    } else {
-        list2.next = mergeTwoLists(list1, list2.next);
+    }
+    else{
+        list2.next= mergeTwoLists(list1,list2.next);
         return list2;
     }
 };
